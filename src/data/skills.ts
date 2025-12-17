cd97a786e25b1b76eda012d01504d32ca02193d2
MartinUTN <martinratti10@gmail.com>
2025-12-17 14:14:28 -0300

    fix: remove docker from skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,5 +2,5 @@ export const skills = {
     languages: ["JavaScript", "TypeScript", "Python", "C#", "SQL"],
     frontend: ["Astro", "React", "Tailwind CSS", "Blazor", "HTML5", "CSS3"],
     backend: ["Node.js", "Express", ".NET WebAPI", "Prisma"],
-    tools: ["Git", "GitHub", "Docker", "VS Code", "Postman"]
+    tools: ["Git", "GitHub", "VS Code", "Postman"]
 };
