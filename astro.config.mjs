a57d40f3ab86e52568f5d1e04ffb17f33dbef1ed
MartinUTN <martinratti10@gmail.com>
2026-02-17 08:46:39 -0300

    Mejora CEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  site: 'https://martinratti.com.ar',
+  integrations: [sitemap()],
 });
